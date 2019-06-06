8be5c38dfa444fbab11996cc5b3ef53cbcb51b45
Dale Roberts <dale.o.roberts@gmail.com>
2019-06-06 11:35:07 +1000

    Remove max_threads

--- hdstats/__init__.py
+++ hdstats/__init__.py
@@ -4,7 +4,7 @@ from .dists import Wishart, ChiSquared, MultivariateNormal, MarchenkoPastur
 from .geomedian import geomedian, nangeomedian
 #from .wishart import sample_wishart
 from .constrained import sample_constrained_gaussian, thresholds_to_constraints
-from .pcm import gm as nangeomedian_pcm, wgm as nanwgeomedian_pcm, emad as emad_pcm, smad as smad_pcm, max_threads
+from .pcm import gm as nangeomedian_pcm, wgm as nanwgeomedian_pcm, emad as emad_pcm, smad as smad_pcm
 from .basic import nancov, gv
 from .twosample import test_sample_means
 from .ts import cosdist, eucdist, completion, discordance, fouriersum, mean_change, median_change, mean_central_diff, complexity, number_peaks, symmetry, mean_abs_change
