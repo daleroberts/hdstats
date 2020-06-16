66a7b94b86c234e69fede0a5cbe9a001059c9fb4
Dale Roberts <dale.o.roberts@gmail.com>
2020-06-16 11:37:55 +1000

    bump version + fast_completion + smooth

---
+++ hdstats/tsslow.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from scipy.signal import wiener
+
+def fast_completion(arr):
+    mask = np.isnan(arr)
+    idx = np.where(~mask, np.arange(mask.shape[-1]), 0)
+    np.maximum.accumulate(idx, axis=-1, out=idx)
+    i, j = np.meshgrid(
+        np.arange(idx.shape[0]), np.arange(idx.shape[1]), indexing="ij"
+    )
+    dat = arr[i[:, :, np.newaxis], j[:, :, np.newaxis], idx]
+    if np.isnan(np.sum(dat[:, :, 0])):
+        fill = np.nanmean(dat, axis=-1)
+        for t in range(dat.shape[-1]):
+            mask = np.isnan(dat[:, :, t])
+            if mask.any():
+                dat[mask, t] = fill[mask]
+            else:
+                break
+    return dat
+
+
+def smooth(arr, k=3):
+    return wiener(arr, (1, 1, k))
--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     extras_require={
         'test': tests_require,
     },
-    version="0.1.3",
+    version="0.1.4",
     description="High-dimensional statistics.",
     url="http://github.com/daleroberts/hdstats",
     author="Dale Roberts",
