59bf2cbac12747a856fe70c815896bd3d9180c97
Kirill Kouzoubov <kirill888@gmail.com>
2020-09-22 16:03:59 +1000

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     extras_require={
         'test': tests_require,
     },
-    version="0.1.5",
+    version="0.1.6",
     description="High-dimensional statistics.",
     url="http://github.com/daleroberts/hdstats",
     author="Dale Roberts",
