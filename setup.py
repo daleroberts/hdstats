9bf46e5eeaa381357e355736fc79c1ff368828f2
Kirill Kouzoubov <kirill888@gmail.com>
2020-11-25 16:53:34 +1100

    Can't go from release to beta, use .post1 syntax instead

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     extras_require={
         'test': tests_require,
     },
-    version="0.1.8b",
+    version="0.1.8.post1",
     description="High-dimensional statistics.",
     url="http://github.com/daleroberts/hdstats",
     author="Dale Roberts",
